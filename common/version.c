86f27fb7ae59374395e186ca57bf98ab651aa6dc
Shiji Yang <yangshiji66@qq.com>
2025-01-09 22:12:52 +0800

    banner: do not show build time and timezone
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

--- common/version.c
+++ common/version.c
@@ -8,8 +8,7 @@
 #include <version.h>
 #include <version_string.h>
 
-#define U_BOOT_VERSION_STRING U_BOOT_VERSION " (" U_BOOT_DATE " - " \
-	U_BOOT_TIME " " U_BOOT_TZ ")" CONFIG_IDENT_STRING
+#define U_BOOT_VERSION_STRING U_BOOT_VERSION " (" U_BOOT_DATE ")" CONFIG_IDENT_STRING
 
 const char version_string[] = U_BOOT_VERSION_STRING;
 const unsigned short version_num = U_BOOT_VERSION_NUM;
